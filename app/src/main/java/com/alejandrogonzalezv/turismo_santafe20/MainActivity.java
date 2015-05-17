ccd8f547b58e608e916e156b7d52f3e476971041
Alejandro <alegv92@gmail.com>
2015-05-17 11:18:47 -0500

    Fragment hoteles  y Splash

--- app/src/main/java/com/alejandrogonzalezv/turismo_santafe20/MainActivity.java
+++ app/src/main/java/com/alejandrogonzalezv/turismo_santafe20/MainActivity.java
@@ -1,5 +1,7 @@
 package com.alejandrogonzalezv.turismo_santafe20;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -27,10 +29,14 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.hoteles) {
+            hoteles fragment = new hoteles();
+            fragmentTransaction.replace(android.R.id.content, fragment).commit();
             return true;
         }
 
